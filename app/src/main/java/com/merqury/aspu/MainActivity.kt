bc67e52325053d848351d34ebb08d743fdc55eae
MerQury14092 <petrakov14092@gmail.com>
2024-02-25 21:12:45 +0300

    Change application icon

--- app/src/main/java/com/merqury/aspu/MainActivity.kt
+++ app/src/main/java/com/merqury/aspu/MainActivity.kt
@@ -10,13 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.merqury.aspu.ui.MainScreen
 import com.merqury.aspu.ui.theme.ASPUTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MainScreen()
         }
     }
 }
