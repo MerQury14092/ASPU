fa05fc85a1488c76ea924f98feb52879fc9d36b9
MerQury14092 <petrakov14092@gmail.com>
2024-02-25 23:23:38 +0300

    Added simple list of news

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 
 dependencies {
     implementation("androidx.navigation:navigation-compose:2.6.0-alpha04")
+    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
