742e1df3cd81a643a0e1467ffd77302d7c8a834c
MerQury14092 <petrakov14092@gmail.com>
2024-02-25 20:01:39 +0300

    Create screen structure

---
+++ app/src/main/java/com/merqury/aspu/ui/navfragments/news.kt
@@ -0,0 +1,8 @@
+package com.merqury.aspu.ui.navfragments
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NewsScreen(){
+
+}
---
+++ app/src/main/java/com/merqury/aspu/ui/navfragments/other.kt
@@ -0,0 +1,7 @@
+package com.merqury.aspu.ui.navfragments
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OtherScreen() {
+}
