27cf921a65d4f1435860dbf9573352ca9ce88bc6
MerQury14092 <petrakov14092@gmail.com>
2024-03-12 23:38:45 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.merqury.aspudev"
+        applicationId = "com.merqury.aspu"
         minSdk = 28
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.03-alpha"
+        versionCode = 11
+        versionName = "1.04-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
