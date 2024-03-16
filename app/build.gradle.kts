0daa2c3ce0e59a3050f94286c3651154d6b20096
MerQury14092 <petrakov14092@gmail.com>
2024-03-16 07:16:11 +0300

    Miss fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.merqury.aspudev"
+        applicationId = "com.merqury.aspu"
         minSdk = 28
         targetSdk = 34
         versionCode = 13
